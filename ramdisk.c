e7c7aac9b91e09862fcb54d1eab8f795e6261255
tyler232 <lchangbo1227@gmail.com>
2024-12-25 20:28:46 -0500

    disk image path customizable

--- ramdisk.c
+++ ramdisk.c
@@ -12,7 +12,7 @@
 #include <ctype.h>
 
 #define DEFAULT_RAMDISK_SIZE (16 * 1024 * 1024) // 16 MB
-#define RAMDISK_IMAGE_PATH "/var/tmp/ramdisk.img"
+#define DEFAULT_RAMDISK_IMAGE_PATH "/var/tmp/ramdisk.img"
 
 size_t parse_size_with_unit(const char *size_str) {
     size_t size = 0;
@@ -51,20 +51,23 @@ void print_help(const char *program_name) {
     printf("Usage: %s [-s size] [-h]\n", program_name);
     printf("Options:\n");
     printf("  -s, --size   Set the RAM disk size (e.g., 16M, 1G)\n");
+    printf("  -p, --path   Set the path for the RAM disk image (default: /var/tmp/ramdisk.img)\n");
     printf("  -h, --help   Display this help message\n");
 }
 
 int main(int argc, char *argv[]) {
     size_t ramdisk_size = DEFAULT_RAMDISK_SIZE;
-
+    const char *ramdisk_image_path = DEFAULT_RAMDISK_IMAGE_PATH;
+    
     static struct option long_options[] = {
         {"size", required_argument, NULL, 's'},
+        {"path", required_argument, NULL, 'p'},
         {"help", no_argument, NULL, 'h'},
         {0, 0, 0, 0}
     };
 
     int opt;
-    while ((opt = getopt_long(argc, argv, "s:h", long_options, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "s:p:h", long_options, NULL)) != -1) {
         switch (opt) {
             case 's': // Custom RAM disk size
                 ramdisk_size = parse_size_with_unit(optarg);
@@ -72,6 +75,9 @@ int main(int argc, char *argv[]) {
                     fprintf(stderr, "Invalid RAM disk size specified.\n");
                     return 1;
                 }
+                break;
+            case 'p': // Custom RAM disk image path
+                ramdisk_image_path = optarg;
                 break;
             case 'h': // print help
                 print_help(argv[0]);
@@ -105,7 +111,7 @@ int main(int argc, char *argv[]) {
     printf("Creating RAM disk of size %zu\n", ramdisk_size);
 
     // Create ramdisk
-    int fd = open(RAMDISK_IMAGE_PATH, O_RDWR | O_CREAT | O_TRUNC, 0644);
+    int fd = open(ramdisk_image_path, O_RDWR | O_CREAT | O_TRUNC, 0644);
     if (fd < 0) {
         perror("Failed to create ramdisk");
         return 1;
@@ -132,9 +138,9 @@ int main(int argc, char *argv[]) {
 
     printf("Ramdisk and partition table created successfully.\n");
 
-    printf("Formatting ramdisk with command: mkfs.ext4 %s\n", RAMDISK_IMAGE_PATH);
+    printf("Formatting ramdisk with command: mkfs.ext4 %s\n", ramdisk_image_path);
     char format_cmd[256];
-    snprintf(format_cmd, sizeof(format_cmd), "mkfs.ext4 %s", RAMDISK_IMAGE_PATH);
+    snprintf(format_cmd, sizeof(format_cmd), "mkfs.ext4 %s", ramdisk_image_path);
     system(format_cmd);
 
     printf("Associating ramdisk with loop device.\n");
